6e890b176a7f7193da2986e20177a6b3208c4e98
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2021-07-17 21:39:24 +0700

    update exercise

--- my first cpp/main.cpp
+++ my first cpp/main.cpp
@@ -6,9 +6,75 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//    int a1;
+//    int a2;
+//    std::cout << "Masukkan angka 1" << std::endl;
+//    std::cin >> a1;
+//    std::cout << "Masukkan angkan 2" << std::endl;
+//    std::cin >> a2;
+//    std::cout << "Hasil" << std::endl;
+//    std::cout << a1 + a2;
+    
+//    int inputAngka;
+//
+//    std::cout << "Masukkan angka" << std::endl;
+//    std::cin >> inputAngka;
+//
+//    int sisaBagi = inputAngka % 2;
+//
+//    if (sisaBagi > 0) {
+//        std::cout << "Bilangan Prima";
+//    } else {
+//        std::cout << "Bukan Bilangan prima";
+//    }
+    
+//    int n;
+//    bool angka_prima = true;
+//
+//    std::cout << "Masukkan angka: ";
+//    std::cin >> n;
+//
+//    if (n == 0 || n == 1) {
+//        angka_prima = false;
+//    } else {
+//        for (int i = 2; i <= n / 2; i++) {
+//            std::cout << i << std::endl;
+//            if (n % i == 0) {
+//                angka_prima = false;
+//                break;
+//            }
+//        }
+//      }
+//
+//    std::cout << std::endl;
+//    if (angka_prima){
+//        std::cout << n << " adalah angka prima";
+//    } else {
+//        std::cout << n << " bukan angka prima";
+//    }
+//    std::cout << std::endl;
+    
+    int panjang, lebar, tinggi;
+    
+    std::cout << "Masukkan panjang : ";
+    std::cin >> panjang;
+    std::cout << "Masukkan lebar : ";
+    std::cin >> lebar;
+    std::cout << "Masukkan tinggi : ";
+    std::cin >> tinggi;
+    std::cout << "Volume balok adalah " << panjang * lebar * tinggi;
+    
+//    for (int i = 1; i < 11; i++) {
+//        if (i % 2 == 0) {
+//            std::cout << i << " bilangan genap" << std::endl;
+//        } else {
+//            std::cout << i << " bilangan ganjil" << std::endl;
+//        }
+//    }
+//
+//    std::cout << std::max(2, 6);
     return 0;
 }
